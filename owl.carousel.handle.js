6d0a4fad69923194e4961de99b75b811690dc7b1
EC2 Default User <ec2-user@ip-172-31-26-80.us-east-2.compute.internal>
2018-04-30 12:10:30 +0000

    first commit

---
+++ owl.carousel.handle.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.owl-carousel').owlCarousel({
+		autoplay: true,
+		autoplayHoverPause: true,
+		autoplayTimeout: 3000,
+		autoplaySpeed: 1000,
+		loop: true,
+		items: 1,
+		thumbs: true,
+		thumbImage: true,
+		thumbContainerClass: 'owl-thumbs',
+		thumbItemClass: 'owl-thumb-item'
+	});
+});
